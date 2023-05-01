246e48a9b64a0cde30ea30b3e74a3c1c01de77ee
boga sravani <boga sravani@SRAVANI>
2023-05-01 08:15:35 +0530

    efds

---
+++ src/test/java/demo/Demo2.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+@Test
+public void demo()
+{
+	System.out.println("demo added successfull");
+	System.out.println("raise a conflict");
+}
+}
---
+++ src/test/java/demo/Sample.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	 public void pull() {
+		System.out.println("pull operation commited succesfull");
+	}
+
+}
